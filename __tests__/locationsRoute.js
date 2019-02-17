20f8aaf2c780e695f615e67c113132df9574626d
Jess <jessdev83@gmail.com>
2019-02-16 23:49:42 -0500

    Locations Tests
    tests written for locations route file, test data file created in dummyData dir.

--- __tests__/locationsRoute.js
+++ __tests__/locationsRoute.js
@@ -1,3 +1,22 @@
 const request = require("supertest");
 const db = require("../data/dbConfig");
-const server = require("./server.js");
+const server = require("../api/server.js");
+const locData = require("../dummyData/locRouteData");
+
+describe("get /locations", () => {
+  beforeEach(() => {
+    return db.migrate.rollback().then(() => {
+      return db.migrate.latest().then(() => {
+        return db.seed.run();
+      });
+    });
+  });
+  it("returns all locations in an array", async () => {
+    const response = await request(server).get("/locations");
+    expect(response.body).toEqual(locData);
+  });
+  it("returns a 200 on success", async () => {
+    const response = await request(server).get("/locations");
+    expect(response.status).toBe(200);
+  });
+});
