c69b763af8095bdaf2b1b24086627e872a82419d
Jess <jessdev83@gmail.com>
2019-02-18 18:57:55 -0500

    Deployment updates

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const server = require("./api/server.js");
 
 const port = process.env.PORT || 5500;
 
+//for heroku deployment
+// const port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
+
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
