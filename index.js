177bd26c40e2b618499e3f432d885cfc1ac5d5cd
Jess <jessdev83@gmail.com>
2019-02-18 19:52:24 -0500

    Changed port variable in index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const server = require("./api/server.js");
 
 //for development
-// const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5500;
 
 //for heroku deployment
-const port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
+// const port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
 
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
