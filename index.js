d6ebe424ac7f8cbaa15134d768b21425004d33b4
Jess <jessdev83@gmail.com>
2019-02-18 19:58:44 -0500

    Made changes to index.js

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ const server = require("./api/server.js");
 //for development
 const port = process.env.PORT || 5500;
 
-//for heroku deployment
+// //for heroku deployment
 // const port = process.env.PORT;
 // if (port == null || port == "") {
 //   port = 8000;
 // }
+
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
 
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
