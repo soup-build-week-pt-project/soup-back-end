0db7e611ffb5df8ce4ebf823b2e5ee94d0579dc2
Jess <jessdev83@gmail.com>
2019-02-18 19:40:36 -0500

    More deployment changes

--- index.js
+++ index.js
@@ -11,26 +11,6 @@ if (port == null || port == "") {
   port = 8000;
 }
 
-const { Client } = require("pg");
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-client.connect();
-
-client.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
-
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
