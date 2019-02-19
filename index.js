3d69dfe6aa478edee1b22626ce68a88c76bd2730
Jess <jessdev83@gmail.com>
2019-02-18 19:32:02 -0500

    Deployment
    trying different things to get Heroku to work.

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ require("dotenv").config();
 
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5500;
+//for development
+// const port = process.env.PORT || 5500;
 
 //for heroku deployment
-// const port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
+const port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
 
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
