84127f0fd9f521362bb33f1a611e66f5051f646f
Jess <jessdev83@gmail.com>
2019-02-20 00:51:19 -0500

    Seed file created for inventory

---
+++ data/Seed_Data/inventory_seeds.js
@@ -0,0 +1,292 @@
+module.exports = [
+  {
+    item: "ORANGES",
+    bw_img: "https://imgur.com/a8FLlRo",
+    color_img: "https://imgur.com/4Xnqlz0",
+    min_quan: 30,
+    location_id: "1",
+    amount: 60,
+    unit: "LBS",
+    category_id: 1
+  },
+  {
+    item: "Peanuts",
+    bw_img: "https://imgur.com/W3RCVKr",
+    color_img: "https://imgur.com/yjZwXsq",
+    min_quan: 10,
+    location_id: "1",
+    amount: 15,
+    unit: "bags",
+    category_id: 11
+  },
+  {
+    item: "Milk",
+    bw_img: "https://imgur.com/RcXuthZ",
+    color_img: "https://imgur.com/U3lgLHh",
+    min_quan: 10,
+    location_id: "1",
+    amount: 15,
+    unit: "gallons",
+    category_id: 3
+  },
+  {
+    item: "Carrots",
+    bw_img: "https://imgur.com/JfDaFIG",
+    color_img: "https://imgur.com/AboMLI8",
+    min_quan: 20,
+    location_id: "1",
+    amount: 30,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Canned Corn",
+    bw_img: "https://imgur.com/euJpEEe",
+    color_img: "https://imgur.com/mxpYpWq",
+    min_quan: 20,
+    location_id: "1",
+    amount: 15,
+    unit: "cans",
+    category_id: 5
+  },
+  {
+    item: "Steak",
+    bw_img: "https://imgur.com/EwUihOa",
+    color_img: "https://imgur.com/BeE7sYW",
+    min_quan: 20,
+    location_id: "1",
+    amount: 25,
+    unit: "lbs",
+    category_id: 6
+  },
+  {
+    item: "Fish",
+    bw_img: "https://imgur.com/QYL4fyx",
+    color_img: "https://imgur.com/W8KEISS",
+    min_quan: 15,
+    location_id: "1",
+    amount: 10,
+    unit: "lbs",
+    category_id: 7
+  },
+  {
+    item: "Coffee",
+    bw_img: "https://imgur.com/AE3rGcy",
+    color_img: "https://imgur.com/tcX4UCu",
+    min_quan: 10,
+    location_id: "1",
+    amount: 20,
+    unit: "lbs",
+    category_id: 2
+  },
+  {
+    item: "Bread",
+    bw_img: "https://imgur.com/2v6VB0E",
+    color_img: "https://imgur.com/OZANUwl",
+    min_quan: 30,
+    location_id: "1",
+    amount: 35,
+    unit: "loaves",
+    category_id: 9
+  },
+  {
+    item: "Frozen Chicken",
+    bw_img: "https://imgur.com/O5cn7U6",
+    color_img: "https://imgur.com/Sj5xo7u",
+    min_quan: 30,
+    location_id: "1",
+    amount: 40,
+    unit: "lbs",
+    category_id: 10
+  },
+  {
+    item: "Carrots",
+    bw_img: "https://imgur.com/JfDaFIG",
+    color_img: "https://imgur.com/AboMLI8",
+    min_quan: 20,
+    location_id: "1",
+    amount: 30,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Canned Corn",
+    bw_img: "https://imgur.com/euJpEEe",
+    color_img: "https://imgur.com/mxpYpWq",
+    min_quan: 20,
+    location_id: "1",
+    amount: 15,
+    unit: "cans",
+    category_id: 5
+  },
+  {
+    item: "Steak",
+    bw_img: "https://imgur.com/EwUihOa",
+    color_img: "https://imgur.com/BeE7sYW",
+    min_quan: 20,
+    location_id: "1",
+    amount: 25,
+    unit: "lbs",
+    category_id: 6
+  },
+  {
+    item: "Fish",
+    bw_img: "https://imgur.com/QYL4fyx",
+    color_img: "https://imgur.com/W8KEISS",
+    min_quan: 15,
+    location_id: "1",
+    amount: 10,
+    unit: "lbs",
+    category_id: 7
+  },
+  {
+    item: "Coffee",
+    bw_img: "https://imgur.com/AE3rGcy",
+    color_img: "https://imgur.com/tcX4UCu",
+    min_quan: 10,
+    location_id: "1",
+    amount: 20,
+    unit: "lbs",
+    category_id: 2
+  },
+  {
+    item: "ORANGES",
+    bw_img: "https://imgur.com/a8FLlRo",
+    color_img: "https://imgur.com/4Xnqlz0",
+    min_quan: 30,
+    location_id: "1",
+    amount: 60,
+    unit: "LBS",
+    category_id: 1
+  },
+  {
+    item: "Peanuts",
+    bw_img: "https://imgur.com/W3RCVKr",
+    color_img: "https://imgur.com/yjZwXsq",
+    min_quan: 10,
+    location_id: "1",
+    amount: 15,
+    unit: "bags",
+    category_id: 11
+  },
+  {
+    item: "Milk",
+    bw_img: "https://imgur.com/RcXuthZ",
+    color_img: "https://imgur.com/U3lgLHh",
+    min_quan: 10,
+    location_id: "1",
+    amount: 15,
+    unit: "gallons",
+    category_id: 3
+  },
+  {
+    item: "Carrots",
+    bw_img: "https://imgur.com/JfDaFIG",
+    color_img: "https://imgur.com/AboMLI8",
+    min_quan: 20,
+    location_id: "1",
+    amount: 30,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Canned Corn",
+    bw_img: "https://imgur.com/euJpEEe",
+    color_img: "https://imgur.com/mxpYpWq",
+    min_quan: 20,
+    location_id: "1",
+    amount: 15,
+    unit: "cans",
+    category_id: 5
+  },
+  {
+    item: "ORANGES",
+    bw_img: "https://imgur.com/a8FLlRo",
+    color_img: "https://imgur.com/4Xnqlz0",
+    min_quan: 30,
+    location_id: "1",
+    amount: 60,
+    unit: "LBS",
+    category_id: 1
+  },
+  {
+    item: "Peanuts",
+    bw_img: "https://imgur.com/W3RCVKr",
+    color_img: "https://imgur.com/yjZwXsq",
+    min_quan: 10,
+    location_id: "1",
+    amount: 15,
+    unit: "bags",
+    category_id: 11
+  },
+  {
+    item: "Milk",
+    bw_img: "https://imgur.com/RcXuthZ",
+    color_img: "https://imgur.com/U3lgLHh",
+    min_quan: 10,
+    location_id: "1",
+    amount: 15,
+    unit: "gallons",
+    category_id: 3
+  },
+  {
+    item: "Carrots",
+    bw_img: "https://imgur.com/JfDaFIG",
+    color_img: "https://imgur.com/AboMLI8",
+    min_quan: 20,
+    location_id: "1",
+    amount: 30,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Canned Corn",
+    bw_img: "https://imgur.com/euJpEEe",
+    color_img: "https://imgur.com/mxpYpWq",
+    min_quan: 20,
+    location_id: "1",
+    amount: 15,
+    unit: "cans",
+    category_id: 5
+  },
+  {
+    item: "Steak",
+    bw_img: "https://imgur.com/EwUihOa",
+    color_img: "https://imgur.com/BeE7sYW",
+    min_quan: 20,
+    location_id: "1",
+    amount: 25,
+    unit: "lbs",
+    category_id: 6
+  },
+  {
+    item: "Fish",
+    bw_img: "https://imgur.com/QYL4fyx",
+    color_img: "https://imgur.com/W8KEISS",
+    min_quan: 15,
+    location_id: "1",
+    amount: 10,
+    unit: "lbs",
+    category_id: 7
+  },
+  {
+    item: "Coffee",
+    bw_img: "https://imgur.com/AE3rGcy",
+    color_img: "https://imgur.com/tcX4UCu",
+    min_quan: 10,
+    location_id: "1",
+    amount: 20,
+    unit: "lbs",
+    category_id: 2
+  },
+  {
+    item: "Bread",
+    bw_img: "https://imgur.com/2v6VB0E",
+    color_img: "https://imgur.com/OZANUwl",
+    min_quan: 30,
+    location_id: "1",
+    amount: 35,
+    unit: "loaves",
+    category_id: 9
+  }
+];
--- data/seeds/03inventory.js
+++ data/seeds/03inventory.js
@@ -1,54 +1,11 @@
+const inventory_seeds = require("../Seed_Data/inventory_seeds");
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("inventory")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("inventory").insert([
-        {
-          id: 1,
-          item: "oranges",
-          amount: 50,
-          unit: "lbs",
-          category_id: 1,
-          location_id: 1,
-          color_img: "https://image.flaticon.com/icons/svg/135/135620.svg",
-          bw_img: "https://image.flaticon.com/icons/svg/135/135389.svg",
-          min_quan: 20
-        },
-        {
-          id: 2,
-          item: "apples",
-          amount: 10,
-          unit: "lbs",
-          category_id: 1,
-          location_id: 2,
-          color_img: "https://image.flaticon.com/icons/svg/135/135620.svg",
-          bw_img: "https://image.flaticon.com/icons/svg/135/135389.svg",
-          min_quan: 20
-        },
-        {
-          id: 3,
-          item: "bananas",
-          amount: 60,
-          unit: "lbs",
-          category_id: 1,
-          location_id: 3,
-          color_img: "https://image.flaticon.com/icons/svg/135/135620.svg",
-          bw_img: "https://image.flaticon.com/icons/svg/135/135389.svg",
-          min_quan: 20
-        },
-        {
-          id: 4,
-          item: "rice",
-          amount: 100,
-          unit: "lbs",
-          category_id: 2,
-          location_id: 1,
-          color_img: "https://image.flaticon.com/icons/svg/135/135620.svg",
-          bw_img: "https://image.flaticon.com/icons/svg/135/135389.svg",
-          min_quan: 40
-        }
-      ]);
+      return knex("inventory").insert(inventory_seeds);
     });
 };
