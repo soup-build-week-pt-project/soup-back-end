7b1bb0c0bf4a3cd09195c4ef94045e3eae42bf96
Jess <jessdev83@gmail.com>
2019-02-21 00:31:32 -0500

    more inventory seeds

--- data/Seed_Data/inventory_seeds.js
+++ data/Seed_Data/inventory_seeds.js
@@ -6,7 +6,7 @@ module.exports = [
     min_quan: 30,
     location_id: 1,
     amount: 60,
-    unit: "LBS",
+    unit: "lbs",
     category_id: 1
   },
   {
@@ -156,7 +156,7 @@ module.exports = [
     min_quan: 30,
     location_id: 3,
     amount: 60,
-    unit: "LBS",
+    unit: "lsb",
     category_id: 1
   },
   {
@@ -206,7 +206,7 @@ module.exports = [
     min_quan: 30,
     location_id: 4,
     amount: 60,
-    unit: "LBS",
+    unit: "lbs",
     category_id: 1
   },
   {
@@ -288,5 +288,225 @@ module.exports = [
     amount: 35,
     unit: "loaves",
     category_id: 9
+  },
+  {
+    item: "Flour",
+    bw_img: "https://i.imgur.com/5Jc55vM.png",
+    color_img: "https://i.imgur.com/TVgAbIy.png",
+    min_quan: 15,
+    location_id: 1,
+    amount: 20,
+    unit: "lbs",
+    category_id: 8
+  },
+  {
+    item: "Sugar",
+    bw_img: "https://i.imgur.com/uxrWPC0.png",
+    color_img: "https://i.imgur.com/h3kBMff.png",
+    min_quan: 15,
+    location_id: 1,
+    amount: 20,
+    unit: "lbs",
+    category_id: 8
+  },
+  {
+    item: "Apples",
+    bw_img: "https://i.imgur.com/Y95TEBZ.png",
+    color_img: "https://i.imgur.com/QVntPEV.png",
+    min_quan: 20,
+    location_id: 1,
+    amount: 12,
+    unit: "lbs",
+    category_id: 1
+  },
+  {
+    item: "Broccoli",
+    bw_img: "https://i.imgur.com/vnIXaTh.png",
+    color_img: "https://i.imgur.com/kBAuyKb.png",
+    min_quan: 10,
+    location_id: 1,
+    amount: 15,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Green Beans",
+    bw_img: "https://i.imgur.com/OE4ucUY.png",
+    color_img: "https://i.imgur.com/F4OYPiU.png",
+    min_quan: 30,
+    location_id: 1,
+    amount: 25,
+    unit: "cans",
+    category_id: 4
+  },
+  {
+    item: "Pork",
+    bw_img: "https://i.imgur.com/pPePCen.png",
+    color_img: "https://i.imgur.com/yk8wkxn.png",
+    min_quan: 15,
+    location_id: 1,
+    amount: 12,
+    unit: "lbs",
+    category_id: 6
+  },
+  {
+    item: "Bagels",
+    bw_img: "https://i.imgur.com/bv1xWyr.png",
+    color_img: "https://i.imgur.com/iC62I9S.png",
+    min_quan: 4,
+    location_id: 1,
+    amount: 5,
+    unit: "dozen",
+    category_id: 9
+  },
+  {
+    item: "Peas",
+    bw_img: "https://i.imgur.com/IZAS9DQ.png",
+    color_img: "https://i.imgur.com/dPloO0u.png",
+    min_quan: 30,
+    location_id: 1,
+    amount: 40,
+    unit: "cans",
+    category_id: 5
+  },
+  {
+    item: "Ice Cream",
+    bw_img: "https://i.imgur.com/callrTz.png",
+    color_img: "https://i.imgur.com/WSqUuLd.png",
+    min_quan: 5,
+    location_id: 1,
+    amount: 4,
+    unit: "gallons",
+    category_id: 10
+  },
+  {
+    item: "Butter",
+    bw_img: "https://i.imgur.com/BmCPyaK.png",
+    color_img: "https://i.imgur.com/dT7q86s.png",
+    min_quan: 10,
+    location_id: 1,
+    amount: 10,
+    unit: "lbs",
+    category_id: 3
+  },
+  {
+    item: "Cheddar Cheese",
+    bw_img: "https://i.imgur.com/nNzxofP.png",
+    color_img: "https://i.imgur.com/S8nkGyM.png",
+    min_quan: 10,
+    location_id: 1,
+    amount: 12,
+    unit: "lbs",
+    category_id: 3
+  },
+  {
+    item: "Pears",
+    bw_img: "https://i.imgur.com/HPDiZki.png",
+    color_img: "https://i.imgur.com/KRDCnMg.png",
+    min_quan: 10,
+    location_id: 1,
+    amount: 15,
+    unit: "lbs",
+    category_id: 1
+  },
+  {
+    item: "Rice",
+    bw_img: "https://i.imgur.com/pHgM7AP.png",
+    color_img: "https://i.imgur.com/6AvGAjF.png",
+    min_quan: 20,
+    location_id: 1,
+    amount: 50,
+    unit: "lbs",
+    category_id: 2
+  },
+  {
+    item: "Cereal",
+    bw_img: "https://i.imgur.com/r8d3gMo.png",
+    color_img: "https://i.imgur.com/U9Sml28.png",
+    min_quan: 10,
+    location_id: 1,
+    amount: 30,
+    unit: "boxes",
+    category_id: 2
+  },
+  {
+    item: "Tomatoes",
+    bw_img: "https://i.imgur.com/OPR20ui.png",
+    color_img: "https://i.imgur.com/87elXkt.png",
+    min_quan: 10,
+    location_id: 1,
+    amount: 15,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Lettuce",
+    bw_img: "https://i.imgur.com/LCI3slC.png",
+    color_img: "https://i.imgur.com/RKk8j7e.png",
+    min_quan: 5,
+    location_id: 1,
+    amount: 10,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Onions",
+    bw_img: "https://i.imgur.com/qSf5Gpr.png",
+    color_img: "https://i.imgur.com/Ka5rCgZ.png",
+    min_quan: 20,
+    location_id: 1,
+    amount: 40,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Green Onions",
+    bw_img: "https://i.imgur.com/jclJ4G1.png",
+    color_img: "https://i.imgur.com/9NmL6ND.png",
+    min_quan: 5,
+    location_id: 1,
+    amount: 2,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Potatoes",
+    bw_img: "https://i.imgur.com/ys6A0pe.png",
+    color_img: "https://i.imgur.com/afsx95o.png",
+    min_quan: 30,
+    location_id: 1,
+    amount: 40,
+    unit: "lbs",
+    category_id: 4
+  },
+  {
+    item: "Grapes",
+    bw_img: "https://i.imgur.com/EAWPWZm.png",
+    color_img: "https://i.imgur.com/qQHpYx2.png",
+    min_quan: 5,
+    location_id: 1,
+    amount: 2,
+    unit: "lbs",
+    category_id: 1
+  },
+  {
+    item: "Peaches",
+    bw_img: "https://i.imgur.com/CBXgGBr.png",
+    color_img: "https://i.imgur.com/XWekc2n.png",
+    min_quan: 20,
+    location_id: 1,
+    amount: 25,
+    unit: "cans",
+    category_id: 5
+  },
+  {
+    item: "Strawberries",
+    bw_img: "https://i.imgur.com/VAFAbOX.png",
+    color_img: "https://i.imgur.com/QLXzeml.png",
+    min_quan: 5,
+    location_id: 1,
+    amount: 10,
+    unit: "lbs",
+    category_id: 1
   }
 ];
