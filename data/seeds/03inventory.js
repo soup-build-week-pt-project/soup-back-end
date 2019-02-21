fa048391e12ed62c51b0284c06d6537a41d76770
Jess <jessdev83@gmail.com>
2019-02-21 01:06:42 -0500

    inventory seeds

--- data/seeds/03inventory.js
+++ data/seeds/03inventory.js
@@ -3,7 +3,7 @@ const inventory_seeds = require("../Seed_Data/inventory_seeds");
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("inventory")
-    .del()
+    .truncate()
     .then(function() {
       // Inserts seed entries
       return knex("inventory").insert(inventory_seeds);
